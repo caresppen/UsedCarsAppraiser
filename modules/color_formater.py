4fa88ab5fca160dafbf6f6d94ae003c11746616d
caresppen <caespe_95@hotmail.com>
2021-05-20 01:26:23 +0200

    Update col trf

--- modules/color_formater.py
+++ modules/color_formater.py
@@ -1,2 +1,21 @@
+import random
+
 def colored(r, g, b, text):
+    '''
+    Color text following a RGB code
+    '''
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
+
+def random_hex_colors(n):
+    '''
+    Define a n-size list of hex-colors
+    '''
+    colorful_list = []
+    
+    for i in range(0, n):
+        random_number = random.randint(0, 16777215)
+        hex_number = str(hex(random_number))
+        hex_number = '#' + hex_number[2:]
+        colorful_list.append(hex_number)
+    
+    return colorful_list
