e40401ac082e64b82c0593c93f8b5ec7c2754f9c
caresppen <caespe_95@hotmail.com>
2021-06-14 00:56:33 +0200

    Update app

--- setup.sh
+++ setup.sh
@@ -1,14 +1,6 @@
-mkdir -p ~/.streamlit/
-
 echo "
-[theme]
-primaryColor = '#439AD6'
-backgroundColor = '#0E1117'
-secondaryBackgroundColor = '#31333F'
-textColor = '#FAFAFA'
-font = 'sans serif'
 [server]
 headless = true
 port = $PORT
 enableCORS = false
-"  > ~/.streamlit/config.toml
+"  >> ~/.streamlit/config.toml
