d98c68faf4f8181eb30c7f6afab7f7da5dca392e
caresppen <caespe_95@hotmail.com>
2021-06-14 01:40:37 +0200

    Update app

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ primaryColor = '#439AD6'
 backgroundColor = '#FFFFFF'
 secondaryBackgroundColor = '#F0F2F6'
 textColor = '#293639'
-font = "sans serif"
+font = 'sans serif'
 [server]
 headless = true
 port = $PORT
